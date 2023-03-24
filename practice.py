1e2ea0c726825b3b8498e47bd335870d24fe3949
Srinu Duda <128722536+Srinududa@users.noreply.github.com>
2023-03-24 12:56:00 +0530

    Click here to the Coading

---
+++ practice.py
@@ -0,0 +1,37 @@
+a=20
+b=20
+c=20
+print(b,c)
+
+
+Name=input("Enter the Name:")
+Roll=int(input("Enter the Roll:"))
+Price=float(input("Enter the Price:"))
+Address=input("Enter the Address:")
+
+print("*"*50)
+print("Name:",Name)
+print("Roll:", Roll)
+#print("Price:", Price)
+print("Address:", Address)
+print("*"*50)
+
+a=20
+print(a)
+
+a=b=c=27
+print(b)
+
+
+srinu=1,2,34
+print(srinu)
+
+P=int(input("Enter the P:"))
+T=int(input("Enter the T:"))
+R=int(input("Enter the R:"))
+I=float(input((P*T*R)/100))
+
+print("P:",P)
+print("T:",T)
+print("R:",R)
+print("I:",I)
